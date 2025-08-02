767f3be37dbc646387285e63e8b9e7f131546cfd
xifong <xifongchristian@gmail.com>
2025-08-02 10:23:09 +0100

    attempting with new function based wrappers

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,7 +1,6 @@
 import { openai } from '@ai-sdk/openai';
-import { PaidAISDKOpenAI } from '@paid-ai/paid-node';
+import { paidStreamText } from '@paid-ai/paid-node';
 import { getClient } from '../utils/client';
-import { StreamTextResult, ToolSet } from 'ai';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -9,24 +8,16 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  debugger;
   const client = await getClient();
 
-  const wrapper = new PaidAISDKOpenAI();
-
-  let result: StreamTextResult<ToolSet, never>;
-  await client.trace(
+  return await client.trace(
     "customer-with-external-id", async () => {
-      result = wrapper.streamText({
+      const result = await paidStreamText({
         model: openai('gpt-4o'),
         messages,
-      })
+      });
+      
+      return result.toDataStreamResponse();
     }
-  )
-
-  if (!result) {
-    throw Error('Failed to stream text');
-  }
-
-  return result.toDataStreamResponse();
+  );
 }
